7084bbf2865df69af8e89d63d2d8fb4a2de29f0f
amorydiaa150 <3bdall3h.diaa@gmail.com>
2026-08-09 19:53:51 +0300

    editing auth

--- src/auth_autho/auth.controller.ts
+++ src/auth_autho/auth.controller.ts
@@ -8,7 +8,7 @@ export class AuthController {
   @Post()
   async signup(@Body(new ValidationPipe()) createAuthDto: CreateAuthDto, @Res() res: any) {
     const result = await this.authService.signup(createAuthDto);
-    res.cookie('token', result.token, { httpOnly: true, secure: true, sameSite: 'none' });
+    res.cookie('token', result.token, { httpOnly: true, secure: true, sameSite: 'lax' });
     return res.status(result.status).json({
       status: result.status,
       message: result.message,
@@ -32,7 +32,7 @@ export class loginController {
     res.cookie('token', result.token, {
       httpOnly: true,
       secure: true,
-      sameSite: 'none',
+      sameSite: 'lax',
     });
     console.log(result)
     return res.status(result.status).json({
