c6441dbebbe8215bdb6aa9b60c687401eff55e94
amorydiaa150 <3bdall3h.diaa@gmail.com>
2026-08-15 12:43:22 +0300

    updare

--- src/manage-providers/manage-providers.controller.ts
+++ src/manage-providers/manage-providers.controller.ts
@@ -25,7 +25,6 @@ export class ManageProvidersController {
 
 
 
-
   @Put(':id')
   @role(["admin"])
   @UseGuards(RoleGuard)
--- src/manage-providers/manage-providers.service.ts
+++ src/manage-providers/manage-providers.service.ts
@@ -4,6 +4,7 @@ import { UpdateManageProviderDto } from './dto/update-manage-provider.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ManageProvidersDocument } from './schema';
+import axios from 'axios';
 @Injectable()
 export class ManageProvidersService {
   constructor(@InjectModel('ManageProviders') private readonly userModel: Model<ManageProvidersDocument>) { }
@@ -22,10 +23,23 @@ export class ManageProvidersService {
 
   async findAll() {
     const data = await this.userModel.find();
-    return data
+
+    const balanceProviders = await Promise.all(
+      data.map(async (provider) => {
+        const res = await axios.post(provider.apiEndpoint, {
+          key: provider.apiKey,
+          action: 'balance',
+        });
+
+        return {
+          ...provider.toObject(),
+          balance: res.data.balance,
+        };
+      }),
+    );
+
+    return balanceProviders;
   }
-
-
 
   async update(id: string, updateManageProviderDto: UpdateManageProviderDto) {
     const data = await this.userModel.findOneAndUpdate({ _id: id }, { ...updateManageProviderDto }, { new: true });
@@ -36,4 +50,6 @@ export class ManageProvidersService {
   async remove(id: string) {
     await this.userModel.findOneAndDelete({ _id: id });
   }
+
+
 }
